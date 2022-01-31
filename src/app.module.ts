604e64b93319fec9c402a638109f362e1ec1eebc
nargok <nargok@gmail.com>
2022-01-31 20:31:57 +0900

    grapqh setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path/posix';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+      typePaths: ['./**/*.gql'],
+      // autoSchemaFile: join(process.cwd(), 'src/schema.gql')
+    }),
+  ],
 })
 export class AppModule {}
